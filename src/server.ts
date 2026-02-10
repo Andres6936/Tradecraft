166c6c6693b4144c2263f85b602607a6239c4497
Andres6936 <andres6936@gmail.com>
2026-02-10 08:56:43 -0500

    Add order property to products

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,59 @@
 const PRODUCTS = {
-  Butter: {
-    Id: 25,
-    Name: "Butter",
-  },
   Computer: {
     Id: 93,
     Name: "Computer",
-  },
-  Furniture: {
-    Id: 49,
-    Name: "Furniture",
-  },
-  Shoes: {
-    Id: 111,
-    Name: "Shoes",
+    Order: 899,
   },
   Smartphone: {
     Id: 94,
     Name: "Smartphone",
+    Order: 540,
+  },
+  Lumber: {
+    Id: 48,
+    Name: "Lumber",
+    Order: 320,
+  },
+  Circuit: {
+    Id: 92,
+    Name: "Circuit",
+    Order: 232,
+  },
+  Furniture: {
+    Id: 49,
+    Name: "Furniture",
+    Order: 227,
+  },
+  Log: {
+    Id: 47,
+    Name: "Log",
+    Order: 152,
+  },
+  Beeswax: {
+    Id: 55,
+    Name: "Beeswax",
+    Order: 111,
+  },
+  Shoes: {
+    Id: 111,
+    Name: "Shoes",
+    Order: 105,
+  },
+  Microchip: {
+    Id: 69,
+    Name: "Microchip",
+    Order: 40,
+  },
+  Butter: {
+    Id: 25,
+    Name: "Butter",
+    Order: 36,
   },
   Sneakers: {
     Id: 113,
     Name: "Sneakers",
+    Order: 20,
   },
 } as const;
 
-export {PRODUCTS}
+export { PRODUCTS };
