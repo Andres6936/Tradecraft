fa3fd8d81884030b997646b206c44c41880de28a
Andres6936 <andres6936@gmail.com>
2026-02-10 13:05:33 -0500

    Add text-to-speech functionality
    Include PowerShell command to speak a message using System.Speech.

--- beep.ts
+++ beep.ts
@@ -1,3 +1,9 @@
 import { play } from "./src/beep";
 
 await play('./public/tone.wav')
+
+import { $ } from 'bun';
+
+const cmd = "Add-Type -AssemblyName System.Speech; $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer; $synth.Speak('Hola, Esperando durante 6624...!');"
+
+await $`powershell ${cmd}`
