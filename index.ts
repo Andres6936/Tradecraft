cfdff2e9d5d77f5b423c07c08ab39640a4c310e0
Andres6936 <andres6936@gmail.com>
2026-02-10 08:46:06 -0500

    Refactor state fetching and order logging
    Replace hardcoded API calls with dynamic fetching from
    playtradecraft.com.
    Update order logging to display product name, side, and quantity.

--- index.ts
+++ index.ts
@@ -1,43 +1,22 @@
-import { getBalance, getBestSellOffer, getOrders, getPriceRange } from "./src/api";
-import { PRODUCTS } from "./src/server";
+const Cookies = process.env.COOKIES || "";
 
-const products = Object.values(PRODUCTS);
-for (const product of products) {
-  console.log("-----------");
+const stream = await fetch(
+  `https://playtradecraft.com/api/state?mineOrdersOnly=1`,
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: Cookies,
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
+    },
+  },
+);
 
-  const [ranges, orders] = await Promise.all([
-    getPriceRange(product.Id),
-    getOrders(product.Id),
-  ]);
+const result = await stream.json();
+const orders = result.orders;
 
-  const balance = getBalance(orders);
-  const bestOfferSell = getBestSellOffer(orders);
-
-  const { Avg, Min, Max } = ranges;
-  console.log(`${product.Name} (${Avg}) / Min: (${Min}) Max: (${Max}) `);
-
-  const { BuyOrdersCount, SellOrdersCount, BuyAmountCount, SellAmountCount } =
-    balance;
-
-  console.log(`Buy (Count): ${BuyOrdersCount}`);
-  console.log(`Buy (Amount): ${BuyAmountCount}`);
-
-  if (bestOfferSell.StatusCode === 201) {
-    const { PriceMarketCount, Amount } = bestOfferSell;
-    console.log(
-      `Sell (Count): ${SellOrdersCount} / [Price Market: ${PriceMarketCount} | Amount: ${Amount}]`,
-    );
-    console.log(`Sell (Amount): ${SellAmountCount}`);
-  } else if (bestOfferSell.StatusCode === 205) {
-    const { BestSellOffer, Amount } = bestOfferSell;
-    console.log(`Sell (Count): ${SellOrdersCount}`);
-    console.log(
-      `Sell (Amount): ${SellAmountCount} / [Best Price: ${BestSellOffer} | Amount: ${Amount}]`,
-    );
-  } else {
-    console.log(`Sell (Count): ${SellOrdersCount}`);
-    console.log(`Sell (Amount): ${SellAmountCount}`);
-  }
-
-  await Bun.sleep(777);
+for (const order of orders) {
+  console.log(
+    `${String(order.productName).padEnd(15, " ")} - ${String(order.side).padStart(4, " ")} - ${String(order.qty.toFixed(0)).padStart(6, " ")}`,
+  );
 }
