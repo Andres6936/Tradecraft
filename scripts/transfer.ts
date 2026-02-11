ccb249f7060dfa9d74485e792747a3841774aa8e
Andres6936 <andres6936@gmail.com>
2026-02-11 11:37:17 -0500

    Format fetch calls and QuestionIsFactoryInspectTransfer

--- scripts/transfer.ts
+++ scripts/transfer.ts
@@ -6,13 +6,16 @@ const regions = [1, 2, 3, 4, 5, 6];
 
 for (const regionId of regions) {
   console.log(`Processing region: ${regionId}`);
-  const stream = await fetch(`https://playtradecraft.com/api/state?regionId=${regionId}`, {
-    headers: {
-      Cookie: Cookies,
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
+  const stream = await fetch(
+    `https://playtradecraft.com/api/state?regionId=${regionId}`,
+    {
+      headers: {
+        Cookie: Cookies,
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
+      },
     },
-  });
+  );
   const payload = await stream.json();
   const tiles = payload.tiles;
 
@@ -25,14 +28,21 @@ for (const regionId of regions) {
       `${tile.productKey} (${tile.kind}) - ${stored.toFixed(1)}/${capacity} (${percentage.toFixed(1)}%)`,
     );
 
-    const [factory, isFactoryInspect] = QuestionIsFactoryInspectTransfer({ key: tile.productKey, kind: tile.kind })
+    const [factory, isFactoryInspect] = QuestionIsFactoryInspectTransfer({
+      key: tile.productKey,
+      kind: tile.kind,
+    });
     if (isFactoryInspect) {
       if (percentage > factory.Tolerance) {
         const percentageExceed = percentage - factory.Tolerance;
-        const amount = Math.floor(((percentageExceed + factory.Offset) / 100) * capacity);
+        const amount = Math.floor(
+          ((percentageExceed + factory.Offset) / 100) * capacity,
+        );
         if (amount < 1) continue;
 
-        console.log(`Tolerance exceeded by ${percentageExceed.toFixed(1)}%, moving ${amount} products`);
+        console.log(
+          `Tolerance exceeded by ${percentageExceed.toFixed(1)}%, moving ${amount} products`,
+        );
         await transferWarehouse({
           regionId: tile.regionId,
           x: tile.x,
@@ -40,11 +50,11 @@ for (const regionId of regions) {
           id: tile.id,
           amount,
         });
-          await Bun.sleep(777);
+        await Bun.sleep(777);
       }
     }
 
-    console.log("----------")
+    console.log("----------");
   }
   await Bun.sleep(777);
 }
