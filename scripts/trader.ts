c69df36cee9b8fed0683a91b00e042870c000c6d
Andres6936 <andres6936@gmail.com>
2026-02-12 10:42:29 -0500

    Add logging for trader sleep durations

--- scripts/trader.ts
+++ scripts/trader.ts
@@ -8,11 +8,13 @@ while (isRunning) {
 
   console.log("Starting buyer");
   await buyer();
+  console.log("Buyer finished, Wait for 37 seconds");
   // Wait for 37 seconds, the cycle is of 30 seconds
   await Bun.sleep(37000)
 
   console.log("Starting seller");
   await seller();
+  console.log("Seller finished, Wait for 2 minutes");
   // Wait for 2 minutes;
   await Bun.sleep(120000)
 }
