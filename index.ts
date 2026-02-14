30174c0596766d41096e4e3b4883f219dd996b6d
Andres6936 <andres6936@gmail.com>
2026-02-14 14:47:59 -0500

    Add order orphan cancellation queue and worker

--- index.ts
+++ index.ts
@@ -5,9 +5,7 @@ import { Queue, Worker } from "bunqueue/client";
 import { main as executeTransfer } from "~/transfer";
 import { buyer } from "~/trader/buyer";
 import { seller } from "~/trader/seller";
-import { getLogger } from "@logtape/logtape";
-
-const logger = getLogger(['trader'])
+import { main as cancelOrdersOrphan } from '~/orders/cancel/orphans'
 
 // --- Transfer Queue and Worker ----
 
@@ -74,6 +72,27 @@ const workerSeller = new Worker(
   "seller",
   async () => {
     await seller();
+    return { statusCode: 200 };
+  },
+  { embedded: true },
+);
+
+// --- Order Orphan Queue and Worker ----
+
+const queueOrderOrphan = new Queue("order-orphan", { embedded: true });
+queueOrderOrphan.setStallConfig({
+  enabled: false,
+});
+
+await queueOrderOrphan.upsertJobScheduler("order-orphan-job", {
+  // Set the job to run every 5 minutes
+  pattern: "*/5 * * * *",
+});
+
+const workerOrderOrphan = new Worker(
+  "order-orphan",
+  async () => {
+    await cancelOrdersOrphan();
     return { statusCode: 200 };
   },
   { embedded: true },
