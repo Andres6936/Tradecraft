7b33669fddff8d33a638ca8b4cf5366da874c3aa
Andres6936 <andres6936@gmail.com>
2026-02-14 13:01:51 -0500

    Refactor seller.ts for readability

--- src/trader/seller.ts
+++ src/trader/seller.ts
@@ -2,17 +2,20 @@ import { getLogger } from "@logtape/logtape";
 import { getOrders, getPriceRange, getState, sendOrder } from "~/api";
 import { ProductsTrade, type ProductTradeType } from "~/server";
 
-const logger = getLogger(['trader', 'seller']);
+const logger = getLogger(["trader", "seller"]);
 
 const ProductsTradeList = Object.values(ProductsTrade);
 
-const sellIf = async (product: ProductTradeType, args: {
-  Me: any,
-  Metrics: any,
-  Inventory: any,
-}) => {
+const sellIf = async (
+  product: ProductTradeType,
+  args: {
+    Me: any;
+    Metrics: any;
+    Inventory: any;
+  },
+) => {
   const { Key, Id, KeepMinInventory } = product;
-  const context = logger.with({ Key })
+  const context = logger.with({ Key });
 
   const productInventoryAmount = args.Inventory[Key];
   if (productInventoryAmount && productInventoryAmount > KeepMinInventory) {
@@ -24,36 +27,42 @@ const sellIf = async (product: ProductTradeType, args: {
       (order) => order.side === "buy" && order.price === null && order.qty >= 1,
     );
     if (marketOrders.length === 0) {
-      return
+      return;
     }
 
     // Get the amount of buy amount of orders
     const buyAmount = marketOrders.reduce((acc, order) => acc + order.qty, 0);
     // Determine the amount of inventory to sell
-    const sellAmount = Math.floor(Math.min(productInventoryAmount - KeepMinInventory, buyAmount));
+    const sellAmount = Math.floor(
+      Math.min(productInventoryAmount - KeepMinInventory, buyAmount),
+    );
     if (sellAmount < 1) {
-      context.info(`[{Key}] Not enough inventory to sell, the amount of ${productInventoryAmount} is less than the minimum required ${KeepMinInventory}`);
+      context.info(
+        `[{Key}] Not enough inventory to sell, the amount of ${productInventoryAmount} is less than the minimum required ${KeepMinInventory}`,
+      );
       return;
     }
 
     const range = await getPriceRange(Id, {
-      withPrecision: 2
+      withPrecision: 2,
     });
 
-    const expectedProfit = Math.floor(sellAmount * (+range.Max));
-    context.info(`[{Key}] Found ${marketOrders.length} market orders with a total of ${buyAmount} units to max. price ${range.Max}, selling ${sellAmount} units, expected profit ${expectedProfit}`);
+    const expectedProfit = Math.floor(sellAmount * +range.Max);
+    context.info(
+      `[{Key}] Found ${marketOrders.length} market orders with a total of ${buyAmount} units to max. price ${range.Max}, selling ${sellAmount} units, expected profit ${expectedProfit}`,
+    );
 
     await sendOrder({
-      orderType: 'limit',
-      side: 'sell',
+      orderType: "limit",
+      side: "sell",
       productId: Id,
       qty: sellAmount,
       price: +range.Max,
       regionId: 1,
       npcAllow: true,
-    })
+    });
   }
-}
+};
 
 const seller = async () => {
   const { Inventory, Me, Metrics } = await getState();
@@ -63,13 +72,13 @@ const seller = async () => {
         Me,
         Metrics,
         Inventory,
-      })
+      });
     } catch (error) {
       console.error(`Error selling ${product.Key}: `, error);
     } finally {
       await Bun.sleep(777);
     }
   }
-}
+};
 
 export { seller };
