474a71966dbc71b9f3cac131034f919c2eb9385d
Andres6936 <andres6936@gmail.com>
2026-02-14 14:53:48 -0500

    Add file logging sink for all loggers

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,8 +1,15 @@
+import { getFileSink } from "@logtape/file";
 import { configureSync, getConsoleSink } from "@logtape/logtape";
 import { getPrettyFormatter } from "@logtape/pretty";
 
 configureSync({
   sinks: {
+    file: getFileSink("./logs/analytics.log", {
+      lazy: true,
+      bufferSize: 8192,
+      flushInterval: 5000,
+      nonBlocking: true,
+    }),
     console: getConsoleSink({
       formatter: getPrettyFormatter({
         timestamp: "time-tz",
@@ -10,9 +17,9 @@ configureSync({
     }),
   },
   loggers: [
-    { category: "trader", lowestLevel: "debug", sinks: ["console"] },
-    { category: "buyer", lowestLevel: "debug", sinks: ["console"] },
-    { category: "seller", lowestLevel: "debug", sinks: ["console"] },
-    { category: "transfer", lowestLevel: "debug", sinks: ["console"] },
+    { category: "trader", lowestLevel: "debug", sinks: ["console", 'file'] },
+    { category: "buyer", lowestLevel: "debug", sinks: ["console", 'file'] },
+    { category: "seller", lowestLevel: "debug", sinks: ["console", 'file'] },
+    { category: "transfer", lowestLevel: "debug", sinks: ["console", 'file'] },
   ],
 });
