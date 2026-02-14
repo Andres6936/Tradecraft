88440879d977e06cb4e6d5cebc7f5eb72b92a4c6
Andres6936 <andres6936@gmail.com>
2026-02-14 10:36:56 -0500

    feat: Add transfer logger config

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,6 +3,7 @@ import { configureSync, getConsoleSink } from "@logtape/logtape";
 configureSync({
   sinks: { console: getConsoleSink() },
   loggers: [
-    { category: "trader", lowestLevel: "debug", sinks: ["console"] }
+    { category: "trader", lowestLevel: "debug", sinks: ["console"] },
+    { category: "transfer", lowestLevel: "debug", sinks: ["console"] }
   ]
 });
