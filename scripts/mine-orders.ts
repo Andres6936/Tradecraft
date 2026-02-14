5352afd9ad707edea419f961d15e36bae6dc8dfb
Andres6936 <andres6936@gmail.com>
2026-02-14 14:05:48 -0500

    Simplify the order fetch

--- scripts/mine-orders.ts
+++ scripts/mine-orders.ts
@@ -1,19 +1,6 @@
-const Cookies = process.env.COOKIES || "";
+import { getMineOrders } from "~/api";
 
-const stream = await fetch(
-  `https://playtradecraft.com/api/state?mineOrdersOnly=1`,
-  {
-    headers: {
-      "Content-Type": "application/json",
-      Cookie: Cookies,
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
-    },
-  },
-);
-
-const result = await stream.json();
-const orders = result.orders;
+const orders = await getMineOrders();
 
 for (const order of orders) {
   console.log(
