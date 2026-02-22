aad87a6c9f8f79bb47b93643a45e64f8d27776bd
Andres6936 <andres6936@gmail.com>
2026-02-22 18:17:47 -0500

    Added shadcn/ui and path aliases

--- web/tradecraft-web/waku.config.ts
+++ web/tradecraft-web/waku.config.ts
@@ -1,14 +1,16 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'waku/config';
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "waku/config";
 
 export default defineConfig({
   vite: {
     plugins: [
+      tsconfigPaths(),
       tailwindcss(),
       react({
         babel: {
-          plugins: ['babel-plugin-react-compiler'],
+          plugins: ["babel-plugin-react-compiler"],
         },
       }),
     ],
