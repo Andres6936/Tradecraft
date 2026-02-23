77a9f6fcf9855bd65abd56706e8555de7df52be8
Andres6936 <andres6936@gmail.com>
2026-02-23 08:56:55 -0500

    Move SelectProduct component to features/main directory

--- web/tradecraft-web/src/view/trader.tsx
+++ web/tradecraft-web/src/view/trader.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { SelectProduct } from "~/components/select/product";
 import { Button } from "~/components/ui/button";
 import {
   Card,
@@ -22,82 +21,86 @@ import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
 
 import { Input } from "~/components/ui/input";
 import { ListOrders } from "~/features/main/components/list-orders";
+import { SelectProduct } from "~/features/main/components/select/product";
+import { TraderContextProvider } from "~/features/main/context/use-trader";
 
 const Trader = () => {
   return (
-    <Card className="w-full min-w-md max-w-md">
-      <CardHeader>
-        <CardTitle>Market Orders</CardTitle>
-        <CardAction>
-          <Label>
-            <p className="text-muted-foreground text-xs">My Orders</p>
-            <Switch />
-          </Label>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <section className="flex flex-col gap-4">
-          <SelectProduct />
-          <hr />
-          <ListOrders />
-          <hr />
-          <div className="flex gap-2 items-center justify-between">
-            <div className="flex flex-col">
-              <p className="text-muted-foreground text-xs text-start">
-                Selected Product
-              </p>
-              <p>Milk</p>
-            </div>
-            <div className="flex flex-col">
-              <p className="text-muted-foreground text-xs text-end">Stock</p>
-              <p>0.00 lts(s)</p>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center justify-between">
-            <div className="flex flex-row gap-2 items-center">
-              <p className="text-muted-foreground text-xs">Quantity</p>
-              <div>
-                <InputGroup>
-                  <InputGroupInput className="max-w-24" />
-                  <InputGroupAddon align="inline-end">
-                    <InputGroupButton>Max</InputGroupButton>
-                  </InputGroupAddon>
-                </InputGroup>
+    <TraderContextProvider>
+      <Card className="w-full min-w-md max-w-md">
+        <CardHeader>
+          <CardTitle>Market Orders</CardTitle>
+          <CardAction>
+            <Label>
+              <p className="text-muted-foreground text-xs">My Orders</p>
+              <Switch />
+            </Label>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <section className="flex flex-col gap-4">
+            <SelectProduct />
+            <hr />
+            <ListOrders />
+            <hr />
+            <div className="flex gap-2 items-center justify-between">
+              <div className="flex flex-col">
+                <p className="text-muted-foreground text-xs text-start">
+                  Selected Product
+                </p>
+                <p>Milk</p>
+              </div>
+              <div className="flex flex-col">
+                <p className="text-muted-foreground text-xs text-end">Stock</p>
+                <p>0.00 lts(s)</p>
               </div>
             </div>
-            <div className="flex flex-row gap-2 items-center">
-              <p className="text-muted-foreground text-xs">Price</p>
-              <Input className="max-w-24" value="107.90" />
+            <div className="flex gap-2 items-center justify-between">
+              <div className="flex flex-row gap-2 items-center">
+                <p className="text-muted-foreground text-xs">Quantity</p>
+                <div>
+                  <InputGroup>
+                    <InputGroupInput className="max-w-24" />
+                    <InputGroupAddon align="inline-end">
+                      <InputGroupButton>Max</InputGroupButton>
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <p className="text-muted-foreground text-xs">Price</p>
+                <Input className="max-w-24" value="107.90" />
+              </div>
             </div>
-          </div>
-          <div className="flex gap-2 items-center justify-between">
-            <Label>
-              <Checkbox />
-              Allow NPC Fill
-            </Label>
+            <div className="flex gap-2 items-center justify-between">
+              <Label>
+                <Checkbox />
+                Allow NPC Fill
+              </Label>
 
-            <div className="flex flex-row gap-2 items-center">
-              <p className="text-muted-foreground text-xs">Total</p>
-              <p>$10.790.0</p>
+              <div className="flex flex-row gap-2 items-center">
+                <p className="text-muted-foreground text-xs">Total</p>
+                <p>$10.790.0</p>
+              </div>
             </div>
-          </div>
 
-          <div className="flex gap-2 items-center justify-between">
-            <ToggleGroup variant="outline" type="single">
-              <ToggleGroupItem value="buy">Buy</ToggleGroupItem>
-              <ToggleGroupItem value="sell">Sell</ToggleGroupItem>
-            </ToggleGroup>
+            <div className="flex gap-2 items-center justify-between">
+              <ToggleGroup variant="outline" type="single">
+                <ToggleGroupItem value="buy">Buy</ToggleGroupItem>
+                <ToggleGroupItem value="sell">Sell</ToggleGroupItem>
+              </ToggleGroup>
 
-            <ToggleGroup variant="outline" type="single">
-              <ToggleGroupItem value="buy">Market</ToggleGroupItem>
-              <ToggleGroupItem value="sell">Limit</ToggleGroupItem>
-            </ToggleGroup>
+              <ToggleGroup variant="outline" type="single">
+                <ToggleGroupItem value="buy">Market</ToggleGroupItem>
+                <ToggleGroupItem value="sell">Limit</ToggleGroupItem>
+              </ToggleGroup>
 
-            <Button>Order</Button>
-          </div>
-        </section>
-      </CardContent>
-    </Card>
+              <Button>Order</Button>
+            </div>
+          </section>
+        </CardContent>
+      </Card>
+    </TraderContextProvider>
   );
 };
 
