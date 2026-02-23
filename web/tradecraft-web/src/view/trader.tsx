c096c414da60686f11344f2cbf1bc622fc453947
Andres6936 <andres6936@gmail.com>
2026-02-22 21:06:06 -0500

    Add radashi dependency and enhance order display

--- web/tradecraft-web/src/view/trader.tsx
+++ web/tradecraft-web/src/view/trader.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React from "react";
+
 import type { ExternOrderType } from "~/types/d";
 
 import { SelectProduct } from "~/components/select/product";
@@ -24,18 +26,39 @@ import { ScrollArea } from "~/components/ui/scroll-area";
 import { Switch } from "~/components/ui/switch";
 import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
 
+import { capitalize } from "radashi";
+
 import state from "~/state.json" with { type: "json" };
 
 const Order = ({ model }: { model: ExternOrderType }) => {
+  const side = React.useMemo(() => {
+    return capitalize(model.side);
+  }, [model]);
+
+  const regionName = React.useMemo(() => {
+    return model.regionName.split(" ").at(0);
+  }, [model]);
+
+  const CompBadge = React.useMemo(() => {
+    const orderType = model.orderType;
+    if (orderType === "market") {
+      return <Badge>Market</Badge>;
+    } else {
+      return <p className="font-bold text-lg">${model.price}</p>;
+    }
+  }, [model]);
+
   return (
     <section className="border rounded py-2 px-3">
       <div className="flex items-center justify-between">
-        <p className="font-bold text-lg">Buy - {model.productName}</p>
-        <Badge>Market</Badge>
+        <p className="font-bold text-lg">
+          {side} - {model.productName}
+        </p>
+        {CompBadge}
       </div>
       <div className="flex items-center justify-between">
         <p className="text-muted-foreground text-xs">
-          {model.qty} {model.unit} - {model.regionName}
+          {model.qty} {model.unit} - {regionName}
         </p>
         <p className="text-muted-foreground text-xs">49s ago</p>
       </div>
