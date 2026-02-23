f09a7cb2fee73873550193ffeda523e9de2bf04a
Andres6936 <andres6936@gmail.com>
2026-02-22 23:12:28 -0500

    Add TanStack React Query for data fetching

--- web/tradecraft-web/src/components/provider.tsx
+++ web/tradecraft-web/src/components/provider.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { ThemeProvider } from "./theme-provider";
+
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: React.PropsWithChildren<{}>) => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 };
