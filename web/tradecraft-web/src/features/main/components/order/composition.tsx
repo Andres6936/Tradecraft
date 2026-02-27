f38aeb175217a2b5c244938c532bf04b84aca522
Andres6936 <andres6936@gmail.com>
2026-02-27 15:29:41 -0500

    Add order indicator and price badge components

--- web/tradecraft-web/src/features/main/components/order/composition.tsx
+++ web/tradecraft-web/src/features/main/components/order/composition.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { capitalize } from "radashi";
+import { Locate } from "lucide-react";
 
+import { Badge } from "~/components/ui/badge";
 import { useOrderContext } from "./context";
 
 const Flex = (props: React.PropsWithChildren<{}>) => (
@@ -25,4 +27,21 @@ const Name = () => {
   );
 };
 
-export { Flex, Row, Name };
+const Indicator = () => {
+  const { isMineOrder } = useOrderContext();
+
+  if (!isMineOrder) return null;
+  return <Locate className="h-4 text-blue-500" />;
+};
+
+const BadgePrice = () => {
+  const { order } = useOrderContext();
+
+  if (order.orderType === "market") {
+    return <Badge>Market</Badge>;
+  } else {
+    return <p className="font-bold text-lg">${order.price}</p>;
+  }
+};
+
+export { Flex, Row, Name, Indicator, BadgePrice };
--- web/tradecraft-web/src/features/main/components/order/index.tsx
+++ web/tradecraft-web/src/features/main/components/order/index.tsx
@@ -1,10 +1,7 @@
 import React, { useMemo } from "react";
-import { capitalize } from "radashi";
 import { type RowComponentProps } from "react-window";
 
-import { Badge } from "~/components/ui/badge";
 import type { ExternOrderType } from "~/types/d";
-import { Locate } from "lucide-react";
 import { TimeAgoCounter } from "./components/time-ago-counter";
 import { OrderContext } from "./context";
 import * as Comp from "./composition";
@@ -23,24 +20,15 @@ const Order = ({
     return model.regionName.split(" ").at(0);
   }, [model]);
 
-  const CompBadge = React.useMemo(() => {
-    const orderType = model.orderType;
-    if (orderType === "market") {
-      return <Badge>Market</Badge>;
-    } else {
-      return <p className="font-bold text-lg">${model.price}</p>;
-    }
-  }, [model]);
-
   return (
     <OrderContext.Provider value={{ order: model, isMineOrder }}>
       <section className="border rounded py-2 px-3" style={style}>
         <Comp.Flex>
           <Comp.Row>
             <Comp.Name />
-            {isMineOrder && <Locate className="h-4 text-blue-500" />}
+            <Comp.Indicator />
           </Comp.Row>
-          {CompBadge}
+          <Comp.BadgePrice />
         </Comp.Flex>
         <Comp.Flex>
           <p className="text-muted-foreground text-xs">
