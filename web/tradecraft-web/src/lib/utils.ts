197b72131ea95932ec44e4d24d405b939da40e8b
Andres6936 <andres6936@gmail.com>
2026-02-27 16:55:28 -0500

    Replace API implementations with external package imports

--- web/tradecraft-web/src/lib/utils.ts
+++ web/tradecraft-web/src/lib/utils.ts
@@ -5,9 +5,4 @@ function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// Ref: https://stackoverflow.com/a/48100007
-const toTruncate = (number: number, digits: number) =>
-  Math.trunc(number * Math.pow(10, digits)) / Math.pow(10, digits);
-
-
-export { cn, toTruncate };
+export { cn };
