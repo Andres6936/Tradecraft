1dda9b7fa8717632970eed00706c5016ba71fb98
Andres6936 <andres6936@gmail.com>
2026-03-01 08:19:09 -0500

    Update history chart lime color from 400 to 500

--- web/tradecraft-web/src/features/main/components/history-chart/hooks.tsx
+++ web/tradecraft-web/src/features/main/components/history-chart/hooks.tsx
@@ -1,6 +1,6 @@
 const useInvertColors = (inverted: boolean) => {
   const firstColor = "var(--color-orange-400)";
-  const secondColor = "var(--color-lime-400)";
+  const secondColor = "var(--color-lime-500)";
   return inverted ? [firstColor, secondColor] : [secondColor, firstColor];
 }
 
