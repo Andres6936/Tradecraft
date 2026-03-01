2d6447568a5887c45a5c4991f957b5f1ffbd1a7f
Andres6936 <andres6936@gmail.com>
2026-02-28 19:59:53 -0500

    Refactor history chart component with skeleton loading states

--- web/tradecraft-web/src/features/main/components/history-chart/index.tsx
+++ web/tradecraft-web/src/features/main/components/history-chart/index.tsx
@@ -14,35 +14,68 @@ import { Label } from "~/components/ui/label";
 import { Switch } from "~/components/ui/switch";
 import { Chart } from "./chart";
 
+const Root = (props: React.PropsWithChildren<{}>) =>
+  <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl" {...props} />
+
+const Header = (props: React.PropsWithChildren<{}>) => {
+  return (
+    <CardHeader>
+      <CardTitle>Market View</CardTitle>
+      {props.children}
+    </CardHeader>
+  )
+}
+
+const SkeletonLoading = (props: React.PropsWithChildren<{}>) => {
+  return (
+    <Root>
+      <Header />
+      <CardContent className="flex flex-1 items-center justify-center">
+        {props.children}
+      </CardContent>
+    </Root>
+  )
+}
 
 const HistoryChart = () => {
   const context = useTraderContext();
 
   if (context.isLoading) {
-    return <p>Loading ...</p>
+    return <SkeletonLoading/>
   }
 
   if (context.error) {
-    return <p>Error: {context.error.message}</p>
+    return (
+      <SkeletonLoading>
+        <p>Error: {context.error.message}</p>
+      </SkeletonLoading>
+    )
   }
 
-  const { productGraph: { Min, Max, Avg, History } } = context;
+  const { isAllProductSelected,  productGraph: { Min, Max, Avg, History } } = context;
+
+  if (isAllProductSelected) {
+    return (
+      <SkeletonLoading>
+        <p>Select a product to show chart</p>
+      </SkeletonLoading>
+    )
+  }
 
   return (
-    <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
-      <CardHeader>
-        <CardTitle>Market View</CardTitle>
+    <Root>
+      <Header>
         <CardAction>
           <Label>
             <p className="text-muted-foreground text-xs">Invert Colors</p>
             <Switch/>
           </Label>
         </CardAction>
-      </CardHeader>
+      </Header>
       <CardContent>
         <Chart avg={Avg} min={Min} max={Max} history={History}/>
       </CardContent>
-    </Card>
+    </Root>
   )
 }
 
