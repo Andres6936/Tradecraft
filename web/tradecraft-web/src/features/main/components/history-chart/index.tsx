69aa5587ed6689542992b75f31ad6b06903dfa16
Andres6936 <andres6936@gmail.com>
2026-02-28 19:49:06 -0500

    Add invert colors toggle to market view chart

--- web/tradecraft-web/src/features/main/components/history-chart/index.tsx
+++ web/tradecraft-web/src/features/main/components/history-chart/index.tsx
@@ -20,6 +20,8 @@ import {
 
 
 import {  useTraderContext } from "~/features/main/context/use-trader";
+import { Label } from "~/components/ui/label";
+import { Switch } from "~/components/ui/switch";
 
 
 const chartConfig = {
@@ -46,6 +48,12 @@ const HistoryChart = () => {
     <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
       <CardHeader>
         <CardTitle>Market View</CardTitle>
+        <CardAction>
+          <Label>
+            <p className="text-muted-foreground text-xs">Invert Colors</p>
+            <Switch/>
+          </Label>
+        </CardAction>
       </CardHeader>
       <CardContent>
         <Chart avg={Avg} min={Min} max={Max} history={History}/>
