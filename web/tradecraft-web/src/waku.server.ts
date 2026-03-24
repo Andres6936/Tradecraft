7157f2052304f8e13ad9f93d653d5be36def9ef4
Andres6936 <andres6936@gmail.com>
2026-03-24 11:24:30 -0500

    Remove Netlify adapter and configuration

--- web/tradecraft-web/src/waku.server.ts
+++
@@ -1,7 +0,0 @@
-import { fsRouter } from 'waku';
-import adapter from 'waku/adapters/netlify';
-
-export default adapter(
-  fsRouter(import.meta.glob('./**/*.{tsx,ts}', { base: './pages' })),
-  { static: true },
-);
