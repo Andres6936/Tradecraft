69f9afcf3bcb236db46eb90d8b5c00b421f21b54
Andres6936 <andres6936@gmail.com>
2026-05-01 22:51:28 -0500

    Add authentication guard and context provider to Properties page

--- web/tradecraft-web/src/view/properties.tsx
+++ web/tradecraft-web/src/view/properties.tsx
@@ -1,31 +1,43 @@
+"use client"
+
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { useLoginContext } from "~/features/login/context/use-login";
 import { ListCard } from "~/features/properties/components/list-card";
+import { PropertiesContextProvider } from "~/features/properties/context/properties-context";
 
 const Properties = () => {
+  const { isAuthenticated, token } = useLoginContext();
+  if (!isAuthenticated) {
+    window.location.href = "/login";
+    return null;
+  }
+
   return (
-    <main className="flex flex-row gap-2">
-      <ListCard/>
+    <PropertiesContextProvider token={token}>
+      <main className="flex flex-row gap-2">
+        <ListCard/>
 
-      <section className="flex flex-col gap-2">
-        <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
-          <CardHeader>
-            <CardTitle>Market View</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <section></section>
-          </CardContent>
-        </Card>
+        <section className="flex flex-col gap-2">
+          <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
+            <CardHeader>
+              <CardTitle>Market View</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <section></section>
+            </CardContent>
+          </Card>
 
-        <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
-          <CardHeader>
-            <CardTitle>Market View</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <section></section>
-          </CardContent>
-        </Card>
-      </section>
-    </main>
+          <Card className="flex flex-1 flex-col w-full min-w-xl max-w-xl">
+            <CardHeader>
+              <CardTitle>Market View</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <section></section>
+            </CardContent>
+          </Card>
+        </section>
+      </main>
+    </PropertiesContextProvider>
   )
 }
 
