e2059e1ef5ec7b201d003beb08f68c03b1a84542
Andres6936 <andres6936@gmail.com>
2026-05-02 11:27:45 -0500

    Add shimmer loading state and error display to list card

--- web/tradecraft-web/src/features/properties/components/list-card.tsx
+++ web/tradecraft-web/src/features/properties/components/list-card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { List } from "react-window";
+import { Shimmer } from 'shimmer-from-structure';
 
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { usePropertiesContext } from "../context/properties-context";
@@ -9,36 +10,64 @@ const ListCard = () => {
   const context = usePropertiesContext();
 
   if (context.isLoading) {
-    return null;
+    return (
+      <Shimmer loading={true}>
+        <Root>
+          <Header />
+          <Content/>
+        </Root>
+      </Shimmer>
+    );
   }
 
   if (context.error) {
-    return null;
+    return (
+      <Root>
+        <Header />
+        <Content>
+          <p className="flex flex-1 justify-center items-center">
+            {context.error.message}
+          </p>
+        </Content>
+      </Root>
+    )
   }
 
   const { tiles } = context;
 
   return (
-    <Card className="w-full min-w-md max-w-md">
-      <CardHeader>
-        <CardTitle>Properties</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <section className="flex flex-col">
-          <div className="relative h-[42rem]">
-            <div className="flex absolute inset-0">
-              <List
-                rowComponent={Property}
-                rowCount={tiles.length}
-                rowHeight={77}
-                rowProps={{ tiles }}
-              />
-            </div>
-          </div>
-        </section>
-      </CardContent>
-    </Card>
+    <Root>
+      <Header/>
+      <Content>
+        <List
+          rowComponent={Property}
+          rowCount={tiles.length}
+          rowHeight={77}
+          rowProps={{ tiles }}
+        />
+      </Content>
+    </Root>
   )
 }
+
+const Root = (props: React.PropsWithChildren<{}>) => <Card className="w-full min-w-md max-w-md" {...props} />
+
+const Header = () => (
+  <CardHeader>
+    <CardTitle>Properties</CardTitle>
+  </CardHeader>
+)
+
+const Content = (props: React.PropsWithChildren<{}>) => (
+  <CardContent>
+    <section className="flex flex-col">
+      <div className="relative h-[42rem]">
+        <div className="flex absolute inset-0">
+          {props.children}
+        </div>
+      </div>
+    </section>
+  </CardContent>
+)
 
 export { ListCard }
