c04a66d94c0177099e78c64d0438027bda01f7c8
Andres6936 <andres6936@gmail.com>
2026-05-02 10:31:29 -0500

    Comment out unused region queries and filter tiles to factories only

--- web/tradecraft-web/src/features/properties/server/actions/get-tiles.ts
+++ web/tradecraft-web/src/features/properties/server/actions/get-tiles.ts
@@ -8,28 +8,28 @@ const getAllTiles = async (options: {token: string}) => {
     getStateWith({
       regionId: 1,
     }, options),
-    getStateWith({
-      regionId: 2,
-    }, options),
-    getStateWith({
-      regionId: 3,
-    }, options),
-    getStateWith({
-      regionId: 4,
-    }, options),
-    getStateWith({
-      regionId: 5,
-    }, options),
-    getStateWith({
-      regionId: 6,
-    }, options),
+    // getStateWith({
+    //   regionId: 2,
+    // }, options),
+    // getStateWith({
+    //   regionId: 3,
+    // }, options),
+    // getStateWith({
+    //   regionId: 4,
+    // }, options),
+    // getStateWith({
+    //   regionId: 5,
+    // }, options),
+    // getStateWith({
+    //   regionId: 6,
+    // }, options),
   ])
 
   if (responses.some(it => isUnautorizedError(it))) {
     return { statusCode: 401, body: { message: "Unauthorized" } } as const
   }
 
-  const tiles = responses.filter(it => it.statusCode === 200).map(it => it.body.tiles).flat();
+  const tiles = responses.filter(it => it.statusCode === 200).map(it => it.body.tiles).flat().filter(it => it.kind === "factory");
   return toSuccess(tiles);
 }
 
